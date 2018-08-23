3f3ff876f935d816f98eef616ea5dcbe79cdcd55
Mauro Delazeri <maurodelazeri@gmail.com>
2018-08-23 10:24:17 -0400

    done

--- security/token/token.go
+++ security/token/token.go
@@ -1,28 +1,49 @@
 package token
 
 import (
+	"errors"
 	"os"
+	"strconv"
 	"time"
 
 	jwt "github.com/dgrijalva/jwt-go"
 )
 
-//https://echo.labstack.com/cookbook/jwt
-//https://github.com/istio/istio/blob/8aa98402bffcdbf0dc18558ae4175529ca4d7dec/security/pkg/platform/gcp.go
-
 // CreateToken create a jwt token
-func CreateToken(account, subject string) (string, error) {
+func CreateToken(account, userid string) (string, error) {
 	now := time.Now()
 	hostname, _ := os.Hostname()
+	expTime, _ := strconv.Atoi(os.Getenv("EXPIRATION_TIME_JWT_TOKEN"))
 	token := jwt.NewWithClaims(jwt.SigningMethodHS512, &jwt.StandardClaims{
-		IssuedAt: now.Unix(),
-		Issuer:   hostname,
-		Subject:  subject,
-		Id:       account,
+		IssuedAt:  now.Unix(),
+		ExpiresAt: time.Now().Add(time.Minute * time.Duration(expTime)).Unix(),
+		Issuer:    hostname,
+		Subject:   userid,
+		Id:        account,
 	})
-	tokenString, err := token.SignedString([]byte(os.Getenv("JWT-TOKEN-SECRET")))
+	tokenString, err := token.SignedString([]byte(os.Getenv("SIGNED_JWT_KEY")))
 	if err != nil {
 		return "", err
 	}
 	return tokenString, nil
+}
+
+// ValidateToken token
+func ValidateToken(token string) (*jwt.StandardClaims, error) {
+	refreshToken, reason := jwt.ParseWithClaims(token, &jwt.StandardClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(os.Getenv("SIGNED_JWT_KEY")), nil
+	})
+	if reason != nil {
+		return nil, reason
+	}
+	if claims, ok := refreshToken.Claims.(*jwt.StandardClaims); ok && refreshToken.Valid {
+		// return new access token and refresh token
+		// id, _ := strconv.Atoi(claims.Subject)
+		// context.JSON(http.StatusOK, gin.H{
+		// 	"refresh_access": libs.GenerateRefreshToken(claims.Email, id),
+		// 	"access_token":   libs.GenerateAccessToken(claims.Email, id),
+		// })
+		return claims, nil
+	}
+	return nil, errors.New("Invalid token")
 }
