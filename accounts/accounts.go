fdcebc0a798c57f4fe1a73074920c707c6f0f5fa
Mauro Delazeri <maurodelazeri@gmail.com>
2018-08-31 19:55:19 -0400

    new stuff

--- accounts/accounts.go
+++ accounts/accounts.go
@@ -6,7 +6,7 @@ import (
 	"sync"
 	"time"
 
-	pb "github.com/maurodelazeri/lion/protobuf/api"
+	pbAPI "github.com/maurodelazeri/lion/protobuf/api"
 	"github.com/sirupsen/logrus"
 
 	"github.com/decillion/go-stm"
@@ -18,7 +18,7 @@ var UserAccounts *Account
 // Account ...
 type Account struct {
 	mutex         sync.RWMutex
-	account       map[pb.Venue]map[string]*stm.TVar
+	account       map[pbAPI.Venue]map[string]*stm.TVar
 	TotalAccounts int
 }
 
@@ -29,7 +29,7 @@ func init() {
 	UserAccounts = new(Account)
 	UserAccounts.mutex.RLock()
 	defer UserAccounts.mutex.RUnlock()
-	UserAccounts.account = make(map[pb.Venue]map[string]*stm.TVar)
+	UserAccounts.account = make(map[pbAPI.Venue]map[string]*stm.TVar)
 	UserAccounts.LoadDataFromDB()
 }
 
@@ -39,19 +39,19 @@ func (m *Account) LoadDataFromDB() {
 	logrus.Info("Loading accounts balances to memory")
 
 	for i := 0; i < 1000; i++ {
-		data := &pb.Account{
+		data := &pbAPI.Account{
 			Id:       time.Now().String(),
 			User:     time.Now().String(),
-			Venue:    pb.Venue_BINANCE,
+			Venue:    pbAPI.Venue_BINANCE,
 			Active:   true,
-			Balances: make(map[string]*pb.Balance),
+			Balances: make(map[string]*pbAPI.Balance),
 		}
 
-		data.Balances[pb.Symbol_BTC.String()] = &pb.Balance{
+		data.Balances[pbAPI.Symbol_BTC.String()] = &pbAPI.Balance{
 			Available: 500000,
 			Hold:      300,
 		}
-		data.Balances[pb.Symbol_USD.String()] = &pb.Balance{
+		data.Balances[pbAPI.Symbol_USD.String()] = &pbAPI.Balance{
 			Available: 500000,
 			Hold:      300,
 		}
@@ -59,19 +59,19 @@ func (m *Account) LoadDataFromDB() {
 		m.LoadDataToMemory(data)
 	}
 
-	data := &pb.Account{
+	data := &pbAPI.Account{
 		Id:       "XXXXXX",
 		User:     "XXXXXX",
-		Venue:    pb.Venue_COINBASEPRO,
+		Venue:    pbAPI.Venue_COINBASEPRO,
 		Active:   true,
-		Balances: make(map[string]*pb.Balance),
+		Balances: make(map[string]*pbAPI.Balance),
 	}
 
-	data.Balances[pb.Symbol_BTC.String()] = &pb.Balance{
+	data.Balances[pbAPI.Symbol_BTC.String()] = &pbAPI.Balance{
 		Available: 500000,
 		Hold:      300,
 	}
-	data.Balances[pb.Symbol_USD.String()] = &pb.Balance{
+	data.Balances[pbAPI.Symbol_USD.String()] = &pbAPI.Balance{
 		Available: 500000,
 		Hold:      300,
 	}
@@ -80,7 +80,7 @@ func (m *Account) LoadDataFromDB() {
 }
 
 // LoadDataToMemory data onto memory
-func (m *Account) LoadDataToMemory(data *pb.Account) {
+func (m *Account) LoadDataToMemory(data *pbAPI.Account) {
 	m.mutex.RLock()
 	defer m.mutex.RUnlock()
 	m.account[data.GetVenue()] = make(map[string]*stm.TVar)
@@ -90,7 +90,7 @@ func (m *Account) LoadDataToMemory(data *pb.Account) {
 }
 
 // ValidateAndUpdateBalances data onto memory
-func (m *Account) ValidateAndUpdateBalances(venue pb.Venue, product pb.Product, account string, amount float64) (*pb.Account, error) {
+func (m *Account) ValidateAndUpdateBalances(venue pbAPI.Venue, product pbAPI.Product, account string, amount float64) (*pbAPI.Account, error) {
 	m.mutex.RLock()
 	defer m.mutex.RUnlock()
 	var err error
@@ -98,7 +98,7 @@ func (m *Account) ValidateAndUpdateBalances(venue pb.Venue, product pb.Product, 
 	if venue, ok := m.account[venue]; ok {
 		if accountNumber, ok := venue[account]; ok {
 			transfer := func(rec *stm.TRec) interface{} {
-				account := rec.Load(accountNumber).(*pb.Account)
+				account := rec.Load(accountNumber).(*pbAPI.Account)
 				symbols := strings.Split(product.String(), "_")
 				if balance, ok := account.Balances[symbols[1]]; ok {
 					if balance.Available >= amount && amount > 0 { // we dont want users sending negative amounts
@@ -107,21 +107,57 @@ func (m *Account) ValidateAndUpdateBalances(venue pb.Venue, product pb.Product, 
 						rec.Store(accountNumber, account)
 					} else {
 						err = errors.New("Balance is not enough to execute this operation")
-						return new(pb.Account)
+						return new(pbAPI.Account)
 					}
 					return account
 				} else {
 					err = errors.New("Venue does not support this product")
-					return new(pb.Account)
+					return new(pbAPI.Account)
 				}
 			}
-			return stm.Atomically(transfer).(*pb.Account), err
+			return stm.Atomically(transfer).(*pbAPI.Account), err
 		} else {
 			err = errors.New("Account was not found")
-			return new(pb.Account), err
+			return new(pbAPI.Account), err
 		}
 	} else {
 		err = errors.New("Balances not found for this venue")
-		return new(pb.Account), err
+		return new(pbAPI.Account), err
+	}
+}
+
+// RefundValues balances to the user account
+func (m *Account) RefundValues(venue pbAPI.Venue, product pbAPI.Product, account string, amount float64) (*pbAPI.Account, error) {
+	m.mutex.RLock()
+	defer m.mutex.RUnlock()
+	var err error
+	// Here is before trading, if things go well, this needs to update again the hold and available to the correct amount based on the execution
+	if venue, ok := m.account[venue]; ok {
+		if accountNumber, ok := venue[account]; ok {
+			transfer := func(rec *stm.TRec) interface{} {
+				account := rec.Load(accountNumber).(*pbAPI.Account)
+				symbols := strings.Split(product.String(), "_")
+				if balance, ok := account.Balances[symbols[1]]; ok {
+
+					// Refund values
+					balance.Available = balance.Available + amount
+					balance.Hold = balance.Hold - amount
+					rec.Store(accountNumber, account)
+
+					return account
+
+				} else {
+					err = errors.New("Venue does not support this product")
+					return new(pbAPI.Account)
+				}
+			}
+			return stm.Atomically(transfer).(*pbAPI.Account), err
+		} else {
+			err = errors.New("Account was not found")
+			return new(pbAPI.Account), err
+		}
+	} else {
+		err = errors.New("Balances not found for this venue")
+		return new(pbAPI.Account), err
 	}
 }
