fd9addb1f42c00e2f7f413077cc44945b21e2a24
Mauro Delazeri <maurodelazeri@gmail.com>
2018-09-16 10:14:38 -0400

    new stuff

--- candle/candle.go
+++ candle/candle.go
@@ -13,17 +13,20 @@ func init() {
 
 }
 
-// Trades is the bucket struck
-type Trades struct {
-	Product  string
-	Exchange string
-	Bucket   *[]*Bucket
+// Venue is the bucket struck
+type Venue struct {
+	Name                 string
+	Product              string
+	Bucket               *[]*Bucket
+	NumMaxBuckets        int
+	IntervalCandle       int
+	HistoryRatesInterval int
 }
 
 // Bucket type
 type Bucket struct {
 	Product  string
-	Exchange string
+	Venue    string
 	Open     float64
 	Close    float64
 	High     float64
@@ -44,29 +47,121 @@ type BucketMessage struct {
 }
 
 // New Initialize the bucket
-func New(exchange string, product string) *Trades {
+func New(venue string, product string) *Venue {
 	buckets := []*Bucket{}
-	s := &Trades{
-		Product:  product,
-		Exchange: exchange,
-		Bucket:   &buckets,
+	s := &Venue{
+		Product:              product,
+		Name:                 venue,
+		Bucket:               &buckets,
+		NumMaxBuckets:        200,
+		IntervalCandle:       1,
+		HistoryRatesInterval: 60,
 	}
 	return s
 }
 
-// ProcessHistoricalBucket one shot update
-func (s *Trades) ProcessHistoricalBucket(buckets *[]*Bucket, bucket *Bucket) {
-	*buckets = append(*buckets, bucket)
-	bucket = (*buckets)[len(*buckets)-1]
+// CandleByDuration create buckets by duration
+func (s *Venue) CandleByDuration(message BucketMessage, buckets *[]*Bucket, candleSize time.Duration, numMaxBuckets int) {
+	t := message.Time.Truncate(candleSize)
+	// If there are no buckets, start one.
+	if len(*buckets) == 0 {
+		*buckets = append(*buckets, &Bucket{
+			Open:     message.Price,
+			Close:    message.Price,
+			Start:    t.UTC(),
+			Low:      math.MaxFloat32,
+			High:     0.0,
+			Duration: candleSize,
+			Volume:   message.Size,
+			Last:     message.Price,
+		})
+	}
+
+	bucket := (*buckets)[len(*buckets)-1]
+	if (*buckets)[len(*buckets)-1].Start.Equal(t) {
+		bucket.Close = message.Price
+		bucket.Last = message.Price
+		// only keep a predefined number max of buckets
+		buckCurrent := len(*buckets)
+		if buckCurrent > numMaxBuckets {
+			removalTotal := buckCurrent - numMaxBuckets
+			if removalTotal > 0 {
+				*buckets = (*buckets)[:0+copy((*buckets)[0:], (*buckets)[removalTotal:])]
+			}
+		}
+	} else {
+		// Time to start a new bucket.
+		*buckets = append(*buckets, &Bucket{
+			Open:     message.Price,
+			Close:    message.Price,
+			Start:    t.UTC(),
+			Low:      math.MaxFloat32,
+			High:     0.0,
+			Duration: candleSize,
+			Volume:   message.Size,
+			Last:     message.Price,
+		})
+		bucket = (*buckets)[len(*buckets)-1]
+	}
+	bucket.Trades++
+	bucket.High = math.Max(bucket.High, message.Price)
+	bucket.Low = math.Min(bucket.Low, message.Price)
+	bucket.Volume = bucket.Volume + message.Size
+	bucket.Last = message.Price
 }
 
-// UpdateBucket each message
-func (s *Trades) UpdateBucket(buckets *[]*Bucket, message BucketMessage) {
-	s.ProcessMessage(message, buckets, time.Duration(intervalCandle)*time.Minute, numMaxBuckets)
+// CandleByNumberOfTrades create buckets by duration
+func (s *Venue) CandleByNumberOfTrades(message BucketMessage, buckets *[]*Bucket, candleSize time.Duration, numMaxBuckets int) {
+	t := message.Time.Truncate(candleSize)
+	// If there are no buckets, start one.
+	if len(*buckets) == 0 {
+		*buckets = append(*buckets, &Bucket{
+			Open:     message.Price,
+			Close:    message.Price,
+			Start:    t.UTC(),
+			Low:      math.MaxFloat32,
+			High:     0.0,
+			Duration: candleSize,
+			Volume:   message.Size,
+			Last:     message.Price,
+		})
+	}
+
+	bucket := (*buckets)[len(*buckets)-1]
+	if (*buckets)[len(*buckets)-1].Start.Equal(t) {
+		bucket.Close = message.Price
+		bucket.Last = message.Price
+		// only keep a predefined number max of buckets
+		buckCurrent := len(*buckets)
+		if buckCurrent > numMaxBuckets {
+			removalTotal := buckCurrent - numMaxBuckets
+			if removalTotal > 0 {
+				*buckets = (*buckets)[:0+copy((*buckets)[0:], (*buckets)[removalTotal:])]
+			}
+		}
+	} else {
+		// Time to start a new bucket.
+		*buckets = append(*buckets, &Bucket{
+			Open:     message.Price,
+			Close:    message.Price,
+			Start:    t.UTC(),
+			Low:      math.MaxFloat32,
+			High:     0.0,
+			Duration: candleSize,
+			Volume:   message.Size,
+			Last:     message.Price,
+		})
+		bucket = (*buckets)[len(*buckets)-1]
+	}
+	bucket.Trades++
+	bucket.High = math.Max(bucket.High, message.Price)
+	bucket.Low = math.Min(bucket.Low, message.Price)
+	bucket.Volume = bucket.Volume + message.Size
+	bucket.Last = message.Price
 }
 
-// ProcessMessage create buckets by duration
-func (s *Trades) ProcessMessage(message BucketMessage, buckets *[]*Bucket, candleSize time.Duration, numMaxBuckets int) {
+// CandleByVolume create buckets by duration
+func (s *Venue) CandleByVolume(message BucketMessage, buckets *[]*Bucket, candleSize time.Duration, numMaxBuckets int) {
 	t := message.Time.Truncate(candleSize)
 	// If there are no buckets, start one.
 	if len(*buckets) == 0 {
