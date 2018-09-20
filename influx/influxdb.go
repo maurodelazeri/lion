2049227058c16f6ca95363809073075b779837d6
Mauro Delazeri <maurodelazeri@gmail.com>
2018-09-20 10:24:50 -0400

    new stuff

--- influx/influxdb.go
+++ influx/influxdb.go
@@ -1,58 +1,34 @@
 package influx
 
 import (
-	"log"
 	"os"
 	"time"
 
 	"github.com/influxdata/influxdb/client/v2"
 )
 
-func aaaaa(name string,tags map[string]string, fields map[string]interface{},t ...time.Time) {
-	// Create a new HTTPClient
-	c, err := client.NewHTTPClient(client.HTTPConfig{
-		Addr:     os.Getenv("INFLUX_HOST"),
-		Username: os.Getenv("INFLUX_USERNAME"),
-		Password: os.Getenv("INFLUX_PASSWORD"),
-	})
+var influxClinet client.Client
+
+func init() {
+	// Make client
+	c, err := client.NewUDPClient(client.UDPConfig{Addr: os.Getenv("INFLUX_UDP_ADDR")})
 	if err != nil {
-		log.Fatal(err)
+		panic(err.Error())
 	}
-	defer c.Close()
-
-	// Create a new point batch
-	bp, err := client.NewBatchPoints(client.BatchPointsConfig{
-		Database:  os.Getenv("INFLUX_DATABASE"),
-		Precision: "ns",
-	})
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Create a point and add to batch
-	tags := map[string]string{"cpu": "cpu-total"}
-	fields := map[string]interface{}{
-		"idle":   10.1,
-		"system": 53.3,
-		"user":   46.6,
-	}
-
-	pt, err := client.NewPoint("cpu_usage", tags, fields, time.Now())
-	if err != nil {
-		log.Fatal(err)
-	}
-	bp.AddPoint(pt)
-
-	// Write the batch
-	if err := c.Write(bp); err != nil {
-		log.Fatal(err)
-	}
-
-	// Close client resources
-	if err := c.Close(); err != nil {
-		log.Fatal(err)
-	}
+	influxClinet = c
 }
 
-export INFLUX_HOST=""
-export INFLUX_USERNAME=""
+// InsertInflux ...
+func InsertInflux(name string, tags map[string]string, fields map[string]interface{}, t ...time.Time) {
+	// Create a new point batch
+	bp, _ := client.NewBatchPoints(client.BatchPointsConfig{
+		Precision: "ns",
+	})
+	pt, err := client.NewPoint(name, tags, fields, time.Now())
+	if err != nil {
+		panic(err.Error())
+	}
+	bp.AddPoint(pt)
+	// Write the batch
+	influxClinet.Write(bp)
+}
