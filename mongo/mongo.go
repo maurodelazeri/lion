7ee9f7b9a0bb0452f5c27158b533bd1473544d90
Mauro Delazeri <maurodelazeri@gmail.com>
2018-10-04 10:19:21 -0400

    new stuff

--- mongo/mongo.go
+++ mongo/mongo.go
@@ -4,8 +4,10 @@ import (
 	"context"
 	"os"
 
+	pbAPI "github.com/maurodelazeri/lion/protobuf/api"
 	"github.com/mongodb/mongo-go-driver/core/connstring"
 	"github.com/mongodb/mongo-go-driver/mongo"
+	"github.com/oleiade/lane"
 	"github.com/sirupsen/logrus"
 )
 
@@ -18,6 +20,9 @@ var (
 	MongoDB *mongo.Database
 	// Client ...
 	Client *mongo.Client
+
+	// MongoQueue ...
+	MongoQueue *lane.Queue
 )
 
 func init() {
@@ -45,4 +50,57 @@ func InitEngine() {
 		os.Exit(1)
 	}
 	MongoDB = Client.Database(dbname, nil)
+}
+
+// InitQueue the operations are in a queue to guarantee the correct execution order
+func InitQueue() {
+	MongoQueue = lane.NewQueue()
+	InitQueue()
+
+	// Let's handle the clients asynchronously
+	go func() {
+		for {
+			for MongoQueue.Head() != nil {
+				item := MongoQueue.Dequeue()
+				Worker(item)
+			}
+		}
+	}()
+}
+
+// Worker execute sequencial execution based on the received instructions
+func Worker(item interface{}) {
+	switch t := item.(type) {
+	case *pbAPI.Trade:
+		// Create a point and add to batch
+		// tags := map[string]string{
+		// 	"venue":   t.GetVenue().String(),
+		// 	"product": t.GetProduct().String(),
+		// 	"side":    t.GetOrderSide().String(),
+		// }
+		// fields := map[string]interface{}{
+		// 	"price":      t.GetPrice(),
+		// 	"size":       t.GetSize(),
+		// 	"side":       pbAPI.OrderType_value[t.GetOrderSide().String()],
+		// 	"venue_type": pbAPI.VenueType_value[t.GetVenueType().String()],
+		// }
+		//InsertInflux("trade", tags, fields, time.Unix(0, int64(t.Timestamp)*int64(time.Nanosecond)))
+	default:
+		logrus.Error("Influx not found a correct type ", t)
+	}
+}
+
+// InsertMongo ...
+func InsertMongo() {
+	// // Create a new point batch
+	// bp, _ := client.NewBatchPoints(client.BatchPointsConfig{
+	// 	Precision: "ns",
+	// })
+	// pt, err := client.NewPoint(name, tags, fields, t)
+	// if err != nil {
+	// 	panic(err.Error())
+	// }
+	// bp.AddPoint(pt)
+	// // Write the batch
+	// influxUDPClinet.Write(bp)
 }
