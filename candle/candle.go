296b6674cd33c012271d3082fecf1f12c278be46
Mauro Delazeri <maurodelazeri@gmail.com>
2018-10-12 15:09:40 -0400

    new stuff

--- candle/candle.go
+++ candle/candle.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/maurodelazeri/concurrency-map-slice"
 	number "github.com/maurodelazeri/go-number"
+	"github.com/maurodelazeri/lion/common"
 	pbAPI "github.com/maurodelazeri/lion/protobuf/api"
 )
 
@@ -13,21 +14,27 @@ import (
 // http://www.blackarbs.com/blog/
 
 var (
-	// SyncCandlestick ...
-	SyncCandlestick *utils.ConcurrentMap
-	// SyncCandlesMap ...
-	SyncCandlesMap *utils.ConcurrentMap
+	// CandlesTime ...
+	CandlesTime *utils.ConcurrentMap
+	// CandlesTick ...
+	CandlesTick *utils.ConcurrentMap
+	// CandlesVolume ...
+	CandlesVolume *utils.ConcurrentMap
+	// CandlesDollar ...
+	CandlesDollar *utils.ConcurrentMap
 )
 
 func init() {
 	// Candlestick ...
-	SyncCandlestick = utils.NewConcurrentMap()
-	// CandlesMap ...
-	SyncCandlesMap = utils.NewConcurrentMap()
+	CandlesTime = utils.NewConcurrentMap()
+	CandlesTick = utils.NewConcurrentMap()
+	CandlesVolume = utils.NewConcurrentMap()
+	CandlesDollar = utils.NewConcurrentMap()
+
 }
 
-// CreateOrUpdateCandleTime ...
-func CreateOrUpdateCandleTime(venue pbAPI.Venue, product pbAPI.Product, price, volume number.Decimal, side pbAPI.Side, createdAt time.Time) {
+// CreateOrUpdateCandleBarTime ...
+func CreateOrUpdateCandleBarTime(venue pbAPI.Venue, product pbAPI.Product, price, volume number.Decimal, side pbAPI.Side, createdAt time.Time) {
 	countBuy := 0
 	countSell := 0
 	if side == pbAPI.Side_BUY {
@@ -35,16 +42,16 @@ func CreateOrUpdateCandleTime(venue pbAPI.Venue, product pbAPI.Product, price, v
 	} else {
 		countSell = 1
 	}
-	for _, g := range pbAPI.Granularity_value {
+	for _, g := range pbAPI.GranularityTime_value {
 		if g != 0 {
 			currentPoint := createdAt.UTC().Truncate(time.Duration(g) * time.Second).Unix()
 			currentKey := fmt.Sprintf("%d:%d:%s:%s", g, currentPoint, venue.String(), product.String())
-			history, exist := SyncCandlestick.Get(currentKey)
+			history, exist := CandlesTime.Get(currentKey)
 			newCandle := &pbAPI.Candle{
 				Venue:       venue,
 				Product:     product,
 				Granularity: g,
-				Point:       int32(currentPoint),
+				Point:       currentPoint,
 				Open:        price.Float64(),
 				Close:       price.Float64(),
 				High:        price.Float64(),
@@ -56,7 +63,7 @@ func CreateOrUpdateCandleTime(venue pbAPI.Venue, product pbAPI.Product, price, v
 				SellTotal:   0,
 			}
 			if exist {
-				historyCandle := history.(*pbAPI.Candle)
+				historyCandle := history.([]*pbAPI.Candle)[len(history.([]*pbAPI.Candle))-1]
 				historyCandle.Open = newCandle.Open
 				if newCandle.High > historyCandle.High {
 					historyCandle.High = newCandle.High
@@ -70,29 +77,174 @@ func CreateOrUpdateCandleTime(venue pbAPI.Venue, product pbAPI.Product, price, v
 				historyCandle.BuyTotal = newCandle.BuyTotal + int32(countBuy)
 				historyCandle.SellTotal = newCandle.SellTotal + int32(countSell)
 			} else {
-				SyncCandlestick.Set(currentKey, newCandle)
-				currentPointsMap, found := SyncCandlesMap.Get(fmt.Sprintf("%d:%s:%s", g, venue.String(), product.String()))
-				if found {
-					SyncCandlesMap.Set(fmt.Sprintf("%d:%s:%s", g, venue.String(), product.String()), append(currentPointsMap.([]int64), currentPoint))
-				} else {
-					SyncCandlesMap.Set(fmt.Sprintf("%d:%s:%s", g, venue.String(), product.String()), append([]int64{}, currentPoint))
-				}
+				CandlesTime.Set(currentKey, append(history.([]*pbAPI.Candle), newCandle))
 			}
 		}
 	}
 }
 
-// CreateOrUpdateCandleTick ...
-func CreateOrUpdateCandleTick(venue pbAPI.Venue, product pbAPI.Product, price, volume number.Decimal, side pbAPI.Side, ticks int32) {
-
+// CreateOrUpdateCandleBarTick ...
+// https://www.thebalance.com/tick-chart-or-1-minute-chart-for-day-trading-1030978
+func CreateOrUpdateCandleBarTick(venue pbAPI.Venue, product pbAPI.Product, price, volume number.Decimal, side pbAPI.Side) {
+	countBuy := 0
+	countSell := 0
+	if side == pbAPI.Side_BUY {
+		countBuy = 1
+	} else {
+		countSell = 1
+	}
+	for _, g := range pbAPI.GranularityTick_value {
+		if g != 0 {
+			newCandle := &pbAPI.Candle{
+				Venue:       venue,
+				Product:     product,
+				Granularity: g,
+				Point:       common.MakeTimestamp(),
+				Open:        price.Float64(),
+				Close:       price.Float64(),
+				High:        price.Float64(),
+				Low:         price.Float64(),
+				Volume:      volume.Float64(),
+				Total:       price.Mul(volume).Float64(),
+				TotalTrades: 1,
+				BuyTotal:    0,
+				SellTotal:   0,
+			}
+			currentKey := fmt.Sprintf("%d:%s:%s", g, venue.String(), product.String())
+			history, exist := CandlesTime.Get(currentKey)
+			if exist {
+				historyCandle := history.([]*pbAPI.Candle)[len(history.([]*pbAPI.Candle))-1]
+				if historyCandle.TotalTrades <= g {
+					historyCandle.Open = newCandle.Open
+					if newCandle.High > historyCandle.High {
+						historyCandle.High = newCandle.High
+					}
+					if newCandle.Low < historyCandle.Low {
+						historyCandle.Low = newCandle.Low
+					}
+					historyCandle.Volume = historyCandle.Volume + newCandle.Volume
+					historyCandle.Total = historyCandle.Total + newCandle.Total
+					historyCandle.TotalTrades = newCandle.TotalTrades + 1
+					historyCandle.BuyTotal = newCandle.BuyTotal + int32(countBuy)
+					historyCandle.SellTotal = newCandle.SellTotal + int32(countSell)
+				} else {
+					// New Candle
+					CandlesTick.Set(currentKey, append(history.([]*pbAPI.Candle), newCandle))
+				}
+			} else {
+				// New Candle
+				CandlesTick.Set(currentKey, append(history.([]*pbAPI.Candle), newCandle))
+			}
+		}
+	}
 }
 
-// CreateOrUpdateCandleVolume ...
-func CreateOrUpdateCandleVolume(venue pbAPI.Venue, product pbAPI.Product, price, volume number.Decimal, side pbAPI.Side, volLimit float64) {
-
+// CreateOrUpdateCandleBarVolume ...
+func CreateOrUpdateCandleBarVolume(venue pbAPI.Venue, product pbAPI.Product, price, volume number.Decimal, side pbAPI.Side, volLimit float64) {
+	countBuy := 0
+	countSell := 0
+	if side == pbAPI.Side_BUY {
+		countBuy = 1
+	} else {
+		countSell = 1
+	}
+	for _, g := range pbAPI.GranularityVolume_value {
+		if g != 0 {
+			newCandle := &pbAPI.Candle{
+				Venue:       venue,
+				Product:     product,
+				Granularity: g,
+				Point:       common.MakeTimestamp(),
+				Open:        price.Float64(),
+				Close:       price.Float64(),
+				High:        price.Float64(),
+				Low:         price.Float64(),
+				Volume:      volume.Float64(),
+				Total:       price.Mul(volume).Float64(),
+				TotalTrades: 1,
+				BuyTotal:    0,
+				SellTotal:   0,
+			}
+			currentKey := fmt.Sprintf("%d:%s:%s", g, venue.String(), product.String())
+			history, exist := CandlesTime.Get(currentKey)
+			if exist {
+				historyCandle := history.([]*pbAPI.Candle)[len(history.([]*pbAPI.Candle))-1]
+				if int32(historyCandle.Total) <= g {
+					historyCandle.Open = newCandle.Open
+					if newCandle.High > historyCandle.High {
+						historyCandle.High = newCandle.High
+					}
+					if newCandle.Low < historyCandle.Low {
+						historyCandle.Low = newCandle.Low
+					}
+					historyCandle.Volume = historyCandle.Volume + newCandle.Volume
+					historyCandle.Total = historyCandle.Total + newCandle.Total
+					historyCandle.TotalTrades = newCandle.TotalTrades + 1
+					historyCandle.BuyTotal = newCandle.BuyTotal + int32(countBuy)
+					historyCandle.SellTotal = newCandle.SellTotal + int32(countSell)
+				} else {
+					// New Candle
+					CandlesTick.Set(currentKey, append(history.([]*pbAPI.Candle), newCandle))
+				}
+			} else {
+				// New Candle
+				CandlesTick.Set(currentKey, append(history.([]*pbAPI.Candle), newCandle))
+			}
+		}
+	}
 }
 
-// CreateOrUpdateCandleDollars ...
-func CreateOrUpdateCandleDollars(venue pbAPI.Venue, product pbAPI.Product, price, volume number.Decimal, side pbAPI.Side, maxDollars float64) {
-
+// CreateOrUpdateCandleBarMoney ...
+func CreateOrUpdateCandleBarMoney(venue pbAPI.Venue, product pbAPI.Product, price, volume number.Decimal, side pbAPI.Side, maxDollars float64) {
+	countBuy := 0
+	countSell := 0
+	if side == pbAPI.Side_BUY {
+		countBuy = 1
+	} else {
+		countSell = 1
+	}
+	for _, g := range pbAPI.GranularityMoney_value {
+		if g != 0 {
+			newCandle := &pbAPI.Candle{
+				Venue:       venue,
+				Product:     product,
+				Granularity: g,
+				Point:       common.MakeTimestamp(),
+				Open:        price.Float64(),
+				Close:       price.Float64(),
+				High:        price.Float64(),
+				Low:         price.Float64(),
+				Volume:      volume.Float64(),
+				Total:       price.Mul(volume).Float64(),
+				TotalTrades: 1,
+				BuyTotal:    0,
+				SellTotal:   0,
+			}
+			currentKey := fmt.Sprintf("%d:%s:%s", g, venue.String(), product.String())
+			history, exist := CandlesTime.Get(currentKey)
+			if exist {
+				historyCandle := history.([]*pbAPI.Candle)[len(history.([]*pbAPI.Candle))-1]
+				if int32(historyCandle.Total) <= g {
+					historyCandle.Open = newCandle.Open
+					if newCandle.High > historyCandle.High {
+						historyCandle.High = newCandle.High
+					}
+					if newCandle.Low < historyCandle.Low {
+						historyCandle.Low = newCandle.Low
+					}
+					historyCandle.Volume = historyCandle.Volume + newCandle.Volume
+					historyCandle.Total = historyCandle.Total + newCandle.Total
+					historyCandle.TotalTrades = newCandle.TotalTrades + 1
+					historyCandle.BuyTotal = newCandle.BuyTotal + int32(countBuy)
+					historyCandle.SellTotal = newCandle.SellTotal + int32(countSell)
+				} else {
+					// New Candle
+					CandlesTick.Set(currentKey, append(history.([]*pbAPI.Candle), newCandle))
+				}
+			} else {
+				// New Candle
+				CandlesTick.Set(currentKey, append(history.([]*pbAPI.Candle), newCandle))
+			}
+		}
+	}
 }
