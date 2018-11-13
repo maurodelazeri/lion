e4b0db0e453e6f7a3a21b9d7deb17c14dc319126
Mauro Delazeri <maurodelazeri@gmail.com>
2018-11-13 06:49:46 -0500

    new stuff

--- venues/okex/websocket.go
+++ venues/okex/websocket.go
@@ -352,6 +352,9 @@ func (r *Websocket) startReading() {
 								}
 								refLiveBook = book
 
+								logrus.Warn("BIDS: ", book.Bids[0].Price, book.Bids[0].Volume)
+								logrus.Warn("ASKS: ", book.Asks[0].Price, book.Asks[0].Volume)
+
 								if r.base.Streaming {
 									serialized, err := proto.Marshal(book)
 									if err != nil {
