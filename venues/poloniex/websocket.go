6344e9702a5a1944918378ae09911e7361cb48d4
Mauro Delazeri <maurodelazeri@gmail.com>
2018-11-17 10:16:56 -0500

    new stuff

--- venues/poloniex/websocket.go
+++ venues/poloniex/websocket.go
@@ -454,7 +454,7 @@ func (r *Websocket) startReading() {
 							}
 							refLiveBook = book
 							//logrus.Warn("BIDS: ", book.Bids[0].Price, book.Bids[0].Volume)
-							//		logrus.Warn("ASKS: ", book.Asks[0].Price, book.Asks[0].Volume)
+							//logrus.Warn("ASKS: ", book.Asks[0].Price, book.Asks[0].Volume)
 
 							if r.base.Streaming {
 								serialized, err := proto.Marshal(book)
