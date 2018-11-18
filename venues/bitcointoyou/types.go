2238395e0d4114a82536ad6b7ab6cac65a7925f6
Mauro Delazeri <maurodelazeri@gmail.com>
2018-11-18 16:22:22 -0500

    new stuff

---
+++ venues/bitcointoyou/types.go
@@ -0,0 +1,31 @@
+package bitcointoyou
+
+// MessageChannel ...
+type MessageChannel struct {
+	M int    `json:"m"`
+	I int    `json:"i"`
+	N string `json:"n"`
+	O string `json:"o"`
+}
+
+// Payload ...
+type Payload struct {
+	OMSID            int    `json:"OMSId,omitempty"`
+	Symbol           string `json:"Symbol,omitempty"`
+	Depth            int    `json:"Depth,omitempty"`
+	IncludeLastCount int    `json:"IncludeLastCount,omitempty"`
+	InstrumentID     int    `json:"InstrumentId,omitempty"`
+}
+
+// Message ...
+type Message struct {
+	M int    `json:"m"`
+	I int    `json:"i"`
+	N string `json:"n"`
+	O string `json:"o"`
+}
+
+// Stream ...
+type Stream struct {
+	Streaming [][]interface{} `json:"streaming"`
+}
