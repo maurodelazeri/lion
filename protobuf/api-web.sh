b47e5a98d1063ec1f0899d62d18c140120f8ec20
Mauro Delazeri <maurodelazeri@gmail.com>
2018-12-04 08:52:44 -0500

    new stuff

--- protobuf/api-web.sh
+++ protobuf/api-web.sh
@@ -1,9 +1,26 @@
 #!/usr/bin/env bash
+
 protoc -I proto/ proto/enum.proto proto/webapi.proto  \
     --go_out=plugins=grpc:../../alaska/api \
     --plugin=protoc-gen-ts=../../siberia/app/node_modules/.bin/protoc-gen-ts \
     --ts_out=service=true:../../siberia/app/src/api \
     --js_out=import_style=commonjs,binary:../../siberia/app/src/api \
 
-sed -i "" '1s/^/\/* eslint-disable *\/ /' ../../siberia/app/src/api/enum_pb.js
-sed -i "" '1s/^/\/* eslint-disable *\/ /' ../../siberia/app/src/api/webapi_pb.js
+
+
+case "$(uname -s)" in
+
+   Darwin)
+     echo 'Mac OS X'
+        sed -i "" '1s/^/\/* eslint-disable *\/ /' ../../siberia/app/src/api/enum_pb.js
+        sed -i "" '1s/^/\/* eslint-disable *\/ /' ../../siberia/app/src/api/webapi_pb.js   
+     ;;
+   Linux)
+     echo 'Linux'
+        sed -i '1s/^/\/* eslint-disable *\/ /' ../../siberia/app/src/api/enum_pb.js
+        sed -i '1s/^/\/* eslint-disable *\/ /' ../../siberia/app/src/api/webapi_pb.js   
+     ;;
+   *)
+     echo 'other OS' 
+     ;;
+esac
