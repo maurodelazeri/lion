a6eef1549f1f65ee432208d2279c1de598d04ef6
Mauro Delazeri <maurodelazeri@gmail.com>
2018-12-03 19:34:49 -0500

    new stuff

--- protobuf/api-compile.sh
+++ protobuf/api-compile.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env bash
+your textn#!/usr/bin/env bash
 protoc -I proto/ proto/enum.proto proto/api.proto --go_out=plugins=grpc:api
 protoc -I proto/ proto/events.proto --go_out=events
--- protobuf/api-web.sh
+++ protobuf/api-web.sh
@@ -4,3 +4,6 @@ protoc -I proto/ proto/enum.proto proto/webapi.proto  \
     --plugin=protoc-gen-ts=../../siberia/app/node_modules/.bin/protoc-gen-ts \
     --ts_out=service=true:../../siberia/app/src/api \
     --js_out=import_style=commonjs,binary:../../siberia/app/src/api \
+
+sed -i "" '1s/^/\/* eslint-disable *\/ /' ../../siberia/app/src/api/enum_pb.js
+sed -i "" '1s/^/\/* eslint-disable *\/ /' ../../siberia/app/src/api/webapi_pb.js
