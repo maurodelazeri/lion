5f233757bde823f5981b2df38cb4f29cf8e61345
Mauro Delazeri <maurodelazeri@gmail.com>
2018-12-08 16:31:17 -0500

    new stuff

--- shared-types/shared_types.go
+++ shared-types/shared_types.go
@@ -2,8 +2,8 @@ package shared
 
 import "github.com/mongodb/mongo-go-driver/bson/objectid"
 
-// MongoVenues ...
-type MongoVenues struct {
+// MongoVenue ...
+type MongoVenue struct {
 	ID         objectid.ObjectID `bson:"_id"`
 	Protobuf   int32             `bson:"protobuf"`
 	Enabled    bool              `bson:"enabled"`
@@ -11,15 +11,19 @@ type MongoVenues struct {
 	APISecret  string            `bson:"api_secret"`
 	Passphrase string            `bson:"passphrase"`
 	Name       string            `bson:"name"`
-	Product    []struct {
-		Product              int32   `bson:"product"`
-		Enabled              bool    `bson:"enabled"`
-		IndividualConnection bool    `bson:"individual_connection"`
-		VenueName            string  `bson:"venue_name"`
-		APIName              string  `bson:"api_name"`
-		MinimumOrdersSize    float64 `bson:"minimum_orders_size"`
-		StepSize             float64 `bson:"step_size"`
-		MakerFee             float64 `bson:"maker_fee"`
-		TakerFee             float64 `bson:"taker_fee"`
-	} `json:"product"`
+}
+
+// MongoProduct ...
+type MongoProduct struct {
+	ID                   objectid.ObjectID `bson:"_id"`
+	VenueID              objectid.ObjectID `bson:"venue_id"`
+	Product              int32             `bson:"product"`
+	Enabled              bool              `bson:"enabled"`
+	IndividualConnection bool              `bson:"individual_connection"`
+	VenueName            string            `bson:"venue_name"`
+	APIName              string            `bson:"api_name"`
+	MinimumOrdersSize    float64           `bson:"minimum_orders_size"`
+	StepSize             float64           `bson:"step_size"`
+	MakerFee             float64           `bson:"maker_fee"`
+	TakerFee             float64           `bson:"taker_fee"`
 }
