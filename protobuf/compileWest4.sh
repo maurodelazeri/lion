be310f4d9fbc464230431a765323cedf325d8b33
Mauro Delazeri <maurodelazeri@gmail.com>
2019-02-02 15:41:15 -0500

    new stuff

--- protobuf/compileWest4.sh
+++ protobuf/compileWest4.sh
@@ -1,34 +1,6 @@
 #!/usr/bin/env bash
 
-rm  ../../siberia/app/src/west4API/*
 rm west4API/*
 
 protoc -I west4API-proto/ west4API-proto/*.proto  \
     --go_out=plugins=grpc:west4API \
-    --plugin=protoc-gen-ts=../../siberia/app/node_modules/.bin/protoc-gen-ts \
-    --ts_out=service=true:../../siberia/app/src/west4API \
-    --js_out=import_style=commonjs,binary:../../siberia/app/src/west4API \
-
-
-case "$(uname -s)" in
-
-   Darwin)
-     echo 'Mac OS X'
-        ls ../../siberia/app/src/west4API/* | while read line;do
-        sed -i "" '1s/^/\/* eslint-disable *\/ /' $line
-        done
-     ;;
-   Linux)
-     echo 'Linux'
-        ls ../../siberia/app/src/west4API/* | while read line;do
-        sed -i '1s/^/\/* eslint-disable *\/ /' $line
-        done
-     ;;
-   *)
-     echo 'other OS' 
-     ;;
-esac
-
-
-
-
